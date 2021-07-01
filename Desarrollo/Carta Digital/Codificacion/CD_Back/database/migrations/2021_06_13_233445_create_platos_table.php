1367831d55d9aab5a3fa31d6522b82d1a43e5dd8
Jairo Jr <jairo.huallpa1@gmail.com>
2021-07-01 12:17:09 -0500

    nuevo backend

--- Desarrollo/Carta Digital/Codificacion/CD_Back/database/migrations/2021_06_13_233445_create_platos_table.php
+++ Desarrollo/Carta Digital/Codificacion/CD_Back/database/migrations/2021_06_13_233445_create_platos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePlatosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('platos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre');
+            $table->string('imagen');
+            $table->double('precio',2,2);
+            $table->string('descripcion');
+            $table->integer('valoracion');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('platos');
     }
 }
