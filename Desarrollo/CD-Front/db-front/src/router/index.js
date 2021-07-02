cf1d00f343b4d1848ff891f434d60aeea6da293b
victorlara123 <victor.lara@unmsm.edu.pe>
2021-07-01 19:25:34 -0500

    vistas login y signup

--- Desarrollo/CD-Front/db-front/src/router/index.js
+++ Desarrollo/CD-Front/db-front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '@/views/Login'
+import Signup from '@/views/Signup'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,10 @@ const routes = [
     path: '/',
     name: 'login',
     component: Login
+  },{
+    path: '/signup',
+    name: 'signup',
+     component: Signup
   }
 ]
 
