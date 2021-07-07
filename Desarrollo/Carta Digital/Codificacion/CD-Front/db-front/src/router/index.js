9a0c1bb7b9eb66653b481759ceccd554475dcf19
ericvipo <ericvpoma@gmail.com>
2021-07-07 15:04:14 -0500

    feat(front admin): ruteo para nuevas vistas

--- Desarrollo/Carta Digital/Codificacion/CD-Front/db-front/src/router/index.js
+++ Desarrollo/Carta Digital/Codificacion/CD-Front/db-front/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Carta from '@/views/Carta'
+import Categories from '@/views/Categories'
+import DayMenu from '@/views/DayMenu'
+import Menues from '@/views/Menues'
+import Promo from '@/views/Promo'
+
 import Login from '@/views/Login'
 
 Vue.use(VueRouter)
@@ -10,7 +16,48 @@ const routes = [
     path: '/',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/daymenu',
+    name: 'daymenu',
+    component: DayMenu,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/carta',
+    name: 'carta',
+    component: Carta,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: Categories,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/menues',
+    name: 'menues',
+    component: Menues,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/promo',
+    name: 'promo',
+    component: Promo,
+    meta: {
+      requiresAuth: true
+    }
   }
+
 ]
 
 const router = new VueRouter({
