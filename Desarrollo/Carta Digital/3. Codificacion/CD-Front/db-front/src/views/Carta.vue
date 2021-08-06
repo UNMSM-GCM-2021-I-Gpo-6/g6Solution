26b35009927b580c519a99192e60848ea89b5c7f
victorlara123 <victor.lara@unmsm.edu.pe>
2021-08-06 15:19:34 -0500

    CD-26 vista de ingreso de informacion de los platos

--- Desarrollo/Carta Digital/3. Codificacion/CD-Front/db-front/src/views/Carta.vue
+++ Desarrollo/Carta Digital/3. Codificacion/CD-Front/db-front/src/views/Carta.vue
@@ -1,5 +1,50 @@
 <template>
-  <div></div>
+  <div>
+     <div class="field">
+        <label class="label">Nombre Plato</label>
+        <div class="control has-icons-left">
+          <input
+            v-model.number="plato.nombre"
+            class="input"
+            type="text"
+            placeholder="ej. Lomo Saltado"
+          />
+          <span class="icon is-small is-left">
+            <BaseIcon name="plato" class="svg-inline" />
+          </span>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Categoria</label>
+        <div class="control has-icons-left">
+          <input
+            v-model.number="plato.categoria"
+            class="input"
+            type="text"
+            placeholder="ej. Plato de fongo"
+          />
+          <span class="icon is-small is-left">
+            <BaseIcon name="plato" class="svg-inline" />
+          </span>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Precio Plato</label>
+        <div class="control has-icons-left">
+          <input
+            v-model.number="plato.precio"
+            class="input"
+            type="text"
+            placeholder="ej. Lomo Saltado"
+          />
+          <span class="icon is-small is-left">
+            <BaseIcon name="plato" class="svg-inline" />
+          </span>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
